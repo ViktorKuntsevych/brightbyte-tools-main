a43540263ff1a55cdb91392b479c71167290eddd
CollegePaul <psmith@rnngroup>
2026-01-13 20:02:08 +0000

    Second Commit

--- password.py
+++ password.py
@@ -1,6 +1,7 @@
 import random
 import string
 
+# Generate Funtion
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     
@@ -8,7 +9,7 @@ def generate_password(length=12):
     
     return password
 
-
+# Outputs
 print("--- Simple Password Generator ---")
 size = int(input("Enter password length: "))
 
